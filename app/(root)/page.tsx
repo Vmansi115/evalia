b51196db6378dc432b493bf575aeeba84e9c89e0
Vmansi115 <vermamk129@gmail.com>
2025-05-15 10:26:23 +0530

    latest

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -16,8 +16,11 @@ const page = async () => {
     await getLatestInterviews({userId: user?.id!})
   ]);
 
-  const hasPastInterviews = userInterviews?.length > 0;
-  const hasUpcomingInterviews = latestInterviews?.length > 0;
+  const hasPastInterviews = Array.isArray(userInterviews) && userInterviews?.length > 0;
+  const hasUpcomingInterviews = Array.isArray(latestInterviews) && latestInterviews?.length > 0;
+
+  
+  
 
   return (
     <>
